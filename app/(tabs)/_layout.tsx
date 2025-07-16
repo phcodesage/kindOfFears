60fc06cc8997548204a6fb31c285d86b5b794cfd
Rechcel Toledo <rechceltoledo@gmail.com>
2025-07-17 06:05:16 +0800

    added: safe area context

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,11 @@
 import { Tabs } from 'expo-router';
 import { Chrome as Home, Grid3x3, List, CircleHelp as HelpCircle, Award } from 'lucide-react-native';
 import { useTheme } from '@/contexts/ThemeContext';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function TabLayout() {
   const { colors } = useTheme();
+  const insets = useSafeAreaInsets();
 
   return (
     <Tabs
@@ -14,8 +16,8 @@ export default function TabLayout() {
         tabBarStyle: {
           backgroundColor: colors.surface,
           borderTopColor: colors.surface,
-          height: 60,
-          paddingBottom: 8,
+          height: 60 + insets.bottom,
+          paddingBottom: 8 + insets.bottom,
           paddingTop: 8,
         },
       }}
